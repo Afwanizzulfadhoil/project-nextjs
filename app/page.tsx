99e2be384a5f2695dded7130ddd8fd39237d2eca
Afwan388 <afwanizzulfadhoil@gmail.com>
2026-07-31 19:13:36 +0700

    feat: update @types/react to version 19.2.18 and enhance homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello, this is made by Vercel and Next.js</h1>
     </div>
   );
