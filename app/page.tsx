cd7938f4ecc159c2bcaa8dccc977d2efb4ac7a1b
Afwan388 <afwanizzulfadhoil@gmail.com>
2026-07-31 19:16:06 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello, this is made by Vercel and Next.js</h1>
+      <h1 className="font-semibold ">Hello, this is made by Vercel and Next.js</h1>
     </div>
   );
 }
