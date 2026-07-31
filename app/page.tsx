607614a8eb1d4f7f61352fa21b8c29fdd273ad0d
Afwan388 <afwanizzulfadhoil@gmail.com>
2026-07-31 19:35:42 +0700

    fit: menambah box

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="w-80 p-5 bg-white rounded-xl shadow-lg">
       <h1 className="font-semibold ">Hello, this is made by Vercel and Next.js</h1>
+      <button className="bg-red-500 text-white px-5 py-2 rounded-lg">Log out</button>
+      <button className="bg-blue-500 text-white px-5 py-2 rounded-lg">Log in</button>
     </div>
   );
 }
